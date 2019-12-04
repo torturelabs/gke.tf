ce54ea63f820b771cfa14f642c1e26ac4200d4d0
Dmitry Ilyin <dima@doty.ru>
2019-12-04 20:34:09 +0300

    Fix true autoscaling

--- main.tf
+++ main.tf
@@ -17,19 +17,10 @@ resource "google_container_cluster" "primary" {
   // node count in each AZ
   initial_node_count = 1
 
-  // autoscaling stuff
-  // https://cloud.google.com/kubernetes-engine/docs/how-to/node-auto-provisioning
-  cluster_autoscaling {
-    enabled = true
-    resource_limits {
-      resource_type = "cpu"
-      maximum       = 10
-    }
-    resource_limits {
-      resource_type = "memory"
-      maximum       = 96
-    }
-  }
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
 
   // use latest supported K8s version
   // don't use that in prod
@@ -60,6 +51,11 @@ resource "google_container_node_pool" "primary_preemtible_nodes" {
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
     ]
+  }
+
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 3
   }
 }
 
