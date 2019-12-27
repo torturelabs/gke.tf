82b67a6fb9128903756c3977d42293ea3346ffd2
Dmitry Ilyin <dima@doty.ru>
2019-12-27 08:26:27 +0300

    Rework disk deletion

--- scripts/del_disks.sh
+++ scripts/del_disks.sh
@@ -1,4 +1,23 @@
 #!/usr/bin/env bash
 
-CMD=$(gcloud compute disks list | grep gke-demo-space | awk '{print "gcloud compute disks delete "$1" --zone "$2" --quiet"}')
-echo $CMD
+DISKS=$(gcloud compute disks list | grep gke-demo-space)
+if [ -z "$DISKS" ]; then
+  echo "Nothing to delete"
+  exit 0
+fi
+
+echo "Make sure that all included disks need to delete:"
+echo "$DISKS" | awk '{print $1}'
+read -p "Do you wish to proceed? (y/n) " yn
+
+case $yn in
+  [Yy]* )
+    echo "Deleting..."
+    CMD=$(echo "$DISKS" | \
+      awk '{print "gcloud compute disks delete "$1" --zone "$2" --quiet && "}')
+    CMD="$CMD echo Completed"
+    #echo $CMD
+    eval $CMD
+    ;;
+  *) echo "Do nothing..."
+esac
